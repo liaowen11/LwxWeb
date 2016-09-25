f2c7176a80d3e837120bb356ebbc7467c9383949
admin <admin@ADMIN-PC>
2016-09-25 15:43:40 +0800

    添加项目文件。

---
+++ LwxWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(LwxWeb.Startup))]
+namespace LwxWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
